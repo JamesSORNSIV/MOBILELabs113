9a4b990650f87f1122a5ce2bfaeb7951b2ad2617
JamesSORNSIV <JamesSORNSIV@gmail.com>
2025-09-05 10:40:19 +0700

    Add PreTest

---
+++ MobileLabPreTest/mobilelabpretestcet113/README.md
@@ -0,0 +1,3 @@
+# mobilelabtestcet113
+
+A new Flutter project.
---
+++ MobileLabPreTest/mobilelabpretestcet113/android/app/src/main/kotlin/com/example/mobilelabtestcet113/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mobilelabtestcet113
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
